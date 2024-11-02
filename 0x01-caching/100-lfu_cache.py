9ad59386e8c0fac48146dfd08974886b93608440
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-11-02 02:42:31 +0000

    LFU Caching advanced

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -6,18 +6,18 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """This is a class that defines LFUCache inheriting from BaseCache"""
+    """This class defines LFUCache inheriting from BaseCaching"""
 
     def __init__(self):
         """Class Constructor"""
         super().__init__()
-        self.frequency = {}  # Track frequency of keys
-        self.order = {}  # Track the order of keys for LRU management
-        self.min_freq = 0  # Track the minimum frequency in use
+        self.freq_map = {}  # Map to track frequency of each key
+        self.lfu_order = []  # List to track the order of keys by frequency
+        self.min_freq = 0  # Track the minimum frequency
 
     def put(self, key, item):
         """
-        This is a method that allows adding a key-value pair to the cache
+        This method allows adding a key-value pair to the cache.
         Args:
             key - the key of the data to be added
             item - the value of the data to be added
@@ -26,22 +26,24 @@ class LFUCache(BaseCaching):
             return
 
         if key in self.cache_data:
-            self.cache_data[key] = item  # Update existing item
+            # Update existing item
+            self.cache_data[key] = item
             self._update_frequency(key)
             return
 
+        # If cache is full, evict least frequently used item
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Evict the least frequently used item
             self._evict()
 
+        # Add new item to cache
         self.cache_data[key] = item
-        self.frequency[key] = 1
-        self.order[key] = len(self.cache_data)  # Use insertion index as order
-        self.min_freq = min(self.min_freq, self.frequency[key])
+        self.freq_map[key] = 1
+        self.lfu_order.append(key)
+        self.min_freq = 1  # Reset min frequency to 1
 
     def get(self, key):
         """
-        This is a method that retrieves an item from the cache by the key.
+        This method retrieves an item from the cache by the key.
         Args:
             key - key to the data to be retrieved.
         Returns:
@@ -50,33 +52,31 @@ class LFUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
 
-        # Update frequency of the key
+        # Update the frequency of the key
         self._update_frequency(key)
         return self.cache_data[key]
 
     def _update_frequency(self, key):
-        """Update the frequency and order of the given key."""
-        freq = self.frequency[key]
-        self.frequency[key] += 1
-        new_freq = self.frequency[key]
+        """Update the frequency of the given key."""
+        freq = self.freq_map[key]
+        self.freq_map[key] += 1
+        new_freq = self.freq_map[key]
 
-        # If this was the only key with the min frequency, update min_freq
-        if (freq == self.min_freq and
-                not any(f == freq for f in self.frequency.values())):
+        # Update LFU order
+        self.lfu_order.remove(key)
+        self.lfu_order.append(key)
+
+        # If was the only key with the minimum frequency, increment min_freq
+        if freq == self.min_freq and not any(f == freq for f in self.freq_map.values()):
             self.min_freq += 1
-
-        # Update the order to reflect the most recently used
-        self.order[key] = len(self.cache_data)  # Update order to the latest
 
     def _evict(self):
         """Evict the least frequently used item from the cache."""
-        lfu_keys = [k for k, v in self.frequency.items() if v == self.min_freq]
+        lfu_keys = [k for k, v in self.freq_map.items() if v == self.min_freq]
 
-        # If there are multiple LFU keys, use LRU to determine which to evict
-        key_to_evict = min(lfu_keys, key=lambda k: self.order[k])
-
-        # Remove the item from cache
-        del self.cache_data[key_to_evict]
-        del self.frequency[key_to_evict]
-        del self.order[key_to_evict]
+        # If there are multiple keys with the same frequency, evict the LRU
+        key_to_evict = lfu_keys[0]  # Pick the first key to evict
+        self.cache_data.pop(key_to_evict)
+        self.freq_map.pop(key_to_evict)
+        self.lfu_order.remove(key_to_evict)
         print(f"DISCARD: {key_to_evict}")
